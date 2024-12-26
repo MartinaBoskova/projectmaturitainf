cbf6d4e40498b9660dc62ce8422d384d2d4b41de
Martina Boskova <mboskova05@gmail.com>
2024-12-26 12:19:51 +0100

    Finding column with names and Lohnarten

--- projectinf_robot.py
+++ projectinf_robot.py
@@ -9,6 +9,9 @@ sheet_obj = wb_obj.active
 row_number = sheet_obj.max_row
 column_number = sheet_obj.max_column
 
+Column_Name = 0
+Column_Lohn = 0
+
 Fall30 = ["ABT SFN st/sv-pfl",
           "AG-Zu.lfd.3(63)",
           "Ant.Listenperis P",
@@ -59,11 +62,38 @@ print("Total Rows:", row_number)
 print("Total Columns:", column_number)
 
 
+def Namecolumn(x):
+    for i in range(1, column_number + 1):
+        name_column = sheet_obj.cell(row=1, column=i)
+        if name_column.value == "Name" or name_column == "name":
+            x = i
+            break
+        else:
+            i = i + 1
+    Column_Name = x
+    if Column_Name == 0:
+        print("Column with Namen not found. Please change the title of the column with names to 'Name or name'.")
+    return Column_Name
+
+def Lohncolumn(x):
+    for i in range(1, column_number + 1):
+        lohn_column = sheet_obj.cell(row=1, column=i)
+        if lohn_column.value == "Lohnartbeschreibung" or lohn_column == "lohnartbeschreibung":
+            x = i
+            break
+        else:
+            i = i + 1
+    Column_Lohn = x
+    if Column_Lohn == 0:
+        print("Column with Lohnartbeschreibungen not found. Please change the title of the column with Lohnartbeschreibungen to 'Lohnartbeschreibung or lohnartbeschreibung'.")
+    return Column_Lohn
+
+
 def people_number(x):
 
     for i in range(2, row_number+1):
-        name_a = sheet_obj.cell(row=i, column=3)
-        name_b = sheet_obj.cell(row=i + 1, column=3)
+        name_a = sheet_obj.cell(row=i, column=Namecolumn(Column_Name))
+        name_b = sheet_obj.cell(row=i + 1, column=Namecolumn(Column_Name))
         if name_a.value == name_b.value:
             i = i + 1
         else:
@@ -74,9 +104,9 @@ def people_number(x):
 
 def fall_30(x, y):
     for i in range(2, row_number+1):
-        Lohnartbeschreibung = sheet_obj.cell(row=i, column=12)
-        name_a = sheet_obj.cell(row=i, column=3)
-        name_b = sheet_obj.cell(row=i + 1, column=3)
+        Lohnartbeschreibung = sheet_obj.cell(row=i, column=Lohncolumn(Column_Lohn))
+        name_a = sheet_obj.cell(row=i, column=Namecolumn(Column_Name))
+        name_b = sheet_obj.cell(row=i + 1, column=Namecolumn(Column_Name))
         if name_a.value == name_b.value and y == 1:
             for j in range(0, len(Fall30)):
                 if Lohnartbeschreibung.value == Fall30[j]:
@@ -96,5 +126,9 @@ def fall_30(x, y):
     print("Number of Fall 30 detected is:", x)
 
 
+Namecolumn(Column_Name)
+print("Column with Namen is number:", Namecolumn(Column_Name))
+Lohncolumn(Column_Lohn)
+print("Column with Lohnartbeschreibungen is number:", Lohncolumn(Column_Lohn))
 people_number(0)
 fall_30(0, 1)
